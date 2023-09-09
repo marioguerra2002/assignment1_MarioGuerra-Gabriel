e5b757b7de62bd7988438cac1942649ec86c22ca
marioguuerra2002 <alu0101395036@ull.edu.es>
2023-09-09 21:10:17 +0200

    fallo en limpieza auxiliar

--- exercise4.java
+++ exercise4.java
@@ -15,45 +15,35 @@ public class exercise4 {
       }
       all_numbers.add(entry_individual);
     }
-    /////
-    // check entry
-    /////
-    for (String numbers : all_numbers) {
-      System.out.print(numbers + " "); // Output user input to check
-    }
-    System.out.println();
     ArrayList<Integer> list = new ArrayList<>();
     list = convertToInt(all_numbers); // Convert the array of strings to an array of integers
-    // bien 
     ArrayList<ArrayList<Integer>> result = new ArrayList<>();
     result = SumOf3(list);
-    // for (int i = 0; i < result.size(); i++) {
-    //   System.out.println(result.get(i));
-    // }
+    for (int i = 0; i < result.size(); i++) {
+      System.out.println(result.get(i));
+    }
+    scan.close();
     
   }
   public static ArrayList<ArrayList<Integer>> SumOf3 (ArrayList<Integer> list) {
     ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-    ArrayList<Integer> auxiliar = new ArrayList<>();
+    // ArrayList<Integer> auxiliar = new ArrayList<>();
     for (int i = 0; i < list.size(); i++) {
       for (int j = i; j < list.size(); j++) {
         for (int k = j; k < list.size(); k++) {
           if (i != j && i != k && j != k) {
             if (list.get(i) + list.get(j) + list.get(k) == 0) {
               // System.out.println(list.get(i) + " " + list.get(j) + " " + list.get(k));
+              ArrayList<Integer> auxiliar = new ArrayList<>(); // hay que optimizar esto porque no puede ser que haya que tener que declarar todo el rato un arraylist
               auxiliar.add(list.get(i));
               auxiliar.add(list.get(j));
               auxiliar.add(list.get(k));
               result.add(auxiliar); // add the arraylist to the arraylist of arraylists
-              // bien 
-              auxiliar.clear();
+              // auxiliar.clear(); // esto es lo que hace que no funcione
             }
           }
         }
       }
-    }
-    for (ArrayList<Integer> numbers : result) {
-      System.out.println(numbers);
     }
     return result;
   }
