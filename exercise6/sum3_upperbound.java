7e9f6e7f727ac6680e1f1637effb11bea9012a68
marioguuerra2002 <alu0101395036@ull.edu.es>
2023-09-11 02:39:47 +0200

    Semioptimización del 4 y esquema del 6

--- exercise6/sum3_upperbound.java
+++ exercise6/sum3_upperbound.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class exercise4 {
+public class sum3_upperbound {
   public static void main (String[] args) {
     Scanner scan = new Scanner(System.in); // Create a Scanner object
     String entry_individual; // Read user input
@@ -18,33 +18,15 @@ public class exercise4 {
     ArrayList<Integer> list = new ArrayList<>();
     list = convertToInt(all_numbers); // Convert the array of strings to an array of integers
     ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-    result = SumOf3(list);
+    result = Sum3UpperBound(list);
     for (int i = 0; i < result.size(); i++) {
       System.out.println(result.get(i));
     }
     scan.close();
-    
   }
-  public static ArrayList<ArrayList<Integer>> SumOf3 (ArrayList<Integer> list) {
+  public static ArrayList<ArrayList<Integer>> Sum3UpperBound (ArrayList<Integer> list) {
     ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-    // ArrayList<Integer> auxiliar = new ArrayList<>();
-    for (int i = 0; i < list.size(); i++) {
-      for (int j = i; j < list.size(); j++) {
-        for (int k = j; k < list.size(); k++) {
-          if (i != j && i != k && j != k) {
-            if (list.get(i) + list.get(j) + list.get(k) == 0) {
-              // System.out.println(list.get(i) + " " + list.get(j) + " " + list.get(k));
-              ArrayList<Integer> auxiliar = new ArrayList<>(); // hay que optimizar esto porque no puede ser que haya que tener que declarar todo el rato un arraylist
-              auxiliar.add(list.get(i));
-              auxiliar.add(list.get(j));
-              auxiliar.add(list.get(k));
-              result.add(auxiliar); // add the arraylist to the arraylist of arraylists
-              // auxiliar.clear(); // esto es lo que hace que no funcione
-            }
-          }
-        }
-      }
-    }
+    
     return result;
   }
   public static ArrayList<Integer> convertToInt (ArrayList<String> all_numbers) {
